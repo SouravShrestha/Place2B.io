12ffb247720127e1f2425609301274dd27d539a4
25644660 <shrestha.sourav30@gmail.com>
2019-12-06 01:22:45 +0530

    Added profile popup

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -2,15 +2,41 @@ import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { DOCUMENT } from "@angular/common";
 import { DataService } from "../data.service";
 import { Router } from '@angular/router';
+import { trigger, transition, animate, style, state} from '@angular/animations'
 
 @Component({
   selector: 'app-nav-bar',
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [ transition(
+          ':enter', 
+          [
+            style({ opacity: 0 }),
+            animate('0.5s ease-out', style({opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            style({ opacity: 1 }),
+            animate('0.5s ease-in', style({opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
 
   light_status:string = 'Lights On';
+  toggle_button_text = 'SIGN UP';
+  _heading = 'Hello, Friend!';
+  _para = 'Enter your personal details and start journey with us';
+  curr_status = 1; // login
+  click_profile = false;
   hover_on_settings = true;
   normal_pos = true;
   img_src = '../../assets/images/set-light.svg';
@@ -68,6 +94,46 @@ export class NavBarComponent implements OnInit {
       document.documentElement.setAttribute('data-theme','dark');
       this.img_src = '../../assets/images/set-light.svg';
       this.data.changeLandSrc('../../assets/images/back.svg');
+    }
+  }
+
+  clicked_profile(){
+    this.curr_status = 1;
+    this.click_profile = true;
+    const scrollY = this.document.documentElement.scrollTop;
+    const sbw = window.innerWidth - document.documentElement.clientWidth;
+    this.document.getElementById("body-main").style.position = 'fixed';
+    this.document.getElementById("upload").style.display = 'none';
+    this.document.getElementById("settings").style.width = '6%';
+    this.document.getElementById("brightness").style.width = '100%';
+    if(this.document.getElementById("temp-ref-main"))
+      this.document.getElementById("temp-ref-main").style.marginRight = sbw+'px';
+    this.document.getElementById("body-main").style.top = '-'+scrollY+'px';
+  }
+
+  close_modal(){
+    const scrollY:number = this.document.getElementById("body-main").offsetTop;
+    this.document.getElementById("body-main").style.position = '';
+    this.document.getElementById("upload").style.display = '';
+    this.document.getElementById("settings").style.width = '';
+    this.document.getElementById("brightness").style.width = '';
+    this.document.getElementById("body-main").style.top = '';
+    if(this.document.getElementById("temp-ref-main"))
+      this.document.getElementById("temp-ref-main").style.marginRight = '0px';
+    window.scrollTo(0, scrollY * -1);
+    this.click_profile = false;
+    console.log(this.click_profile);
+  }
+
+  toggle_modal_view(){
+    if(this.curr_status == 1){ //login
+      this.curr_status = 0;
+      this.document.getElementById('right-column').style.transform = "translateX(-100%)";
+      this.document.getElementById('left-column').style.transform = "translateX(+100%)";
+    }else{ //sign up
+      this.curr_status = 1;
+      this.document.getElementById('right-column').style.transform = "translateX(0%)";
+      this.document.getElementById('left-column').style.transform = "translateX(0%)";
     }
   }
 
